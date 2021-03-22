19321750d28f7cb028893c5593c9810d3e8d7399
drovosek30 <63120516+drovosek30@users.noreply.github.com>
2021-03-22 17:19:45 +0200

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,4 +5,5 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
-java -jar target/${NAME}-${VERSION}.jar
+#java -jar target/${NAME}-${VERSION}.jar
+java -jar target/my-app-1.0-SNAPSHOT.jar
